8f7f14f04627ffbb6bccc1b993b0eee32c41abb2
benzelenskiy <ben.zelenskiy@gmail.com>
2025-09-26 17:03:58 -0400

    Update SNO.pde.py

--- SNO.pde.py
+++ SNO.pde.py
@@ -1,17 +1,53 @@
+
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+import Chebyshev
 import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 import os
 import time
 from timeit import default_timer
-from Beam_SNO.D2Utilities3 import *
-from Beam_SNO.D2Adam import Adam
+from D2utilities3 import *
+from D2Adam import Adam
 import time
 import math
 import scipy.special as sp
+import warnings
+warnings.simplefilter('ignore', np.exceptions.RankWarning)
+'''def get_grid(f):
+    x_k = torch.linspace(0, degree-2, degree-1, dtype=torch.double, device=f.device)
+    x_n = -torch.cos(torch.pi*(x_k+.5)/degree)
+    x = (f.shape[-1]-1)*.01+(f.shape[-1]-1)*.01*x_n
+    f = torch.index_select(f,-1,x.long())
+    return f
+def fct(f):
+    # Reverse rows like f(end:-1:1,:) in MATLAB
+    f = f.flip(dims=(2,))
+
+    # Get dimensions
+    A = f.shape
+    N = A[2]
+    a = torch.zeros_like(f, dtype=float)
+    a = math.sqrt(2 / N) * dct(f,norm=None)
+    a[0, :, :] /= math.sqrt(2)
+
+    return a
+def ifct(a, l=None):
+    # Get dimensions
+    k = a.shape
+    N = k[0]
+
+    # Adjust scaling: first row multiplied by sqrt(2), others unchanged
+    a[:,:,0] = math.sqrt(2) * a[:,:,0]
+
+    # Apply inverse DCT (along axis 0)
+    f = idct(math.sqrt(N / 2) * a,norm=None)
+    # Reverse rows like f(end:-1:1,:) in MATLAB
+    f = f.flip(dims=(2,))
+
+    return f'''
 # ====================================
 # saving settings
 # ====================================
@@ -29,7 +65,7 @@ if not os.path.exists(save_results_to):
 # ====================================
 
 class Sumudu_Transform(nn.Module):
-    def __init__(self, in_channels, out_channels, width, s):
+    def __init__(self, in_channels, out_channels, degree, width, s):
         super(Sumudu_Transform, self).__init__()
         
         self.degree = degree
@@ -37,183 +73,158 @@ class Sumudu_Transform(nn.Module):
         self.s = s
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.coefficient = torch.zeros((self.degree))
 
 
+
+        self.flip = (-1)**torch.randint(0,2,(in_channels, out_channels))
         self.scale = (1 / (in_channels*out_channels))
-        self.weight1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, degree, dtype=torch.double))
+        self.weight1 = nn.Parameter((self.scale*torch.rand((in_channels, out_channels), dtype=torch.float64)))
+        self.weight2 = nn.Parameter((self.scale*torch.rand((in_channels, out_channels), dtype=torch.float64)))
 
     def coefficient_training(self, input, degree):
-        output = torch.zeros((input.shape[0], degree), dtype= torch.double,device=input.device)
-        amplitude = input.shape[0]
-        variable_amp = input[:,0,1].unsqueeze(1)/(.05*math.sin(.05))
-        input_size = input.shape[1]*input.shape[2]//degree
-        input = input.reshape(-1, input.shape[1]*input.shape[2])
-        input = input[:,:(degree+1)].permute(1,0)
-        input_np = input.detach().cpu().numpy()
-        n = np.arange(degree+1)
-        m = np.arange(degree+1).reshape(-1,1)
-        binoms = sp.comb(m,n)
-        signs = (-1)**(n+m)
-        numerator = (signs*binoms)@input_np
-        denominator = (.0025*input_size)**n
-        output = numerator.transpose(1,0)/denominator.flatten()
-        output = torch.from_numpy(output).to(device='cuda', dtype=torch.double)
-        output = torch.mul(variable_amp, output)
-
-
-
-
-        return output
-    def coefficient_validation(self, input, degree):
-        output = torch.zeros((input.shape[0], degree), dtype= torch.double,device=input.device)
-        amplitude = input.shape[0]
-        variable_amp = torch.zeros(amplitude, device=input.device)
-        for i in range(amplitude):
-            variable_amp[i] = math.floor(input[i,0,1].item()/(math.exp(-.05)*math.sin(.05)*.05))
-        variable_amp = variable_amp.unsqueeze(1)
-        for i in range(degree):
-            output[:,i] = (((-.05+5j)**i)).imag
-        output = torch.mul(output, variable_amp)
+        output = input.reshape(input.shape[0]*input.shape[1]*input.shape[2], input.shape[3]).permute(1,0)
+        output = torch.from_numpy(np.polyfit(np.linspace(0, (s-1)*.02, s, dtype=np.float64), output.detach().cpu().numpy(), degree-1)).to('cuda').float().permute(1,0)
+        output = output.reshape(input.shape[0], input.shape[1], input.shape[2], degree)    
         
+
         return output
+    def transform(self, input):
+        fact = torch.from_numpy(sp.factorial(np.linspace(0, input.shape[3]-1, input.shape[3], dtype=np.float64))).to('cuda').flip((0,))
+        output = torch.mul(input, fact)
+        return output
+    def inverse_transform(self, input):
+        ##input = input.reshape(-1, input.shape[1]*input.shape[2]).detach().cpu().numpy()
+        ##output = np.zeros((input.shape[0], s*width), dtype= np.double)
+        ##for i in range(input.shape[0]):
+        ##    output[i, :] = chebyshev.cheby_idct(input[i,:])
+        ##factorial = torch.from_numpy(sp.factorial(np.linspace(0,s*width-1, s*width, dtype= np.double))).to('cuda').double()
+        ##output = torch.from_numpy(output).to('cuda').double()
+        ##output = torch.mul(output, factorial)
+        ##output = ifct(input)
+        fact = torch.from_numpy(sp.factorial(np.linspace(0, input.shape[3]-1, input.shape[3], dtype=np.float64))).to('cuda').flip((0,))
+        output = torch.div(input, fact)
+        return output
+
     def approximate_sum(self, width, input):
-        discretization = torch.linspace(0, (s)*.01, steps = s*width, dtype=torch.double, device=input.device)
-        row_powers = torch.linspace(1, input.shape[1], steps = input.shape[1], dtype=torch.double, device=input.device).unsqueeze(1)
+        '''discretization = torch.linspace(0, (s)*.01, steps = s, dtype=torch.float, device=input.device)
+        row_powers = torch.linspace(1, input.shape[2], steps = input.shape[2], dtype=torch.float, device=input.device).unsqueeze(1)
         discretization = discretization**row_powers
-        output = torch.einsum("dx,ad->ax", discretization, input)
+        output = torch.einsum("aid,dx->aix", input, discretization)
         output = output.reshape(input.shape[0], s, width)
         output = output.permute(0,2,1)
-
+        output = torch.zeros((input.shape[0], width, s), dtype= torch.float, device=input.device)
+        for i in range(input.shape[0]):
+            for j in range(width):
+                output[i,j,:] = torch.from_numpy(chebyshev.cheby_sum(np.linspace(0, (s-1)*.01, s, dtype=float),input[i,j,:].detach().cpu().numpy(), 0,((s-1)*.01))).to('cuda').float()
+'''     
+        x = np.linspace(0, (s-1)*.02, s, dtype=np.float64)
+        output = torch.zeros(input.shape[0], input.shape[1], input.shape[2], s, dtype= torch.float64, device=input.device)
+        for i in range(input.shape[0]):
+            for k in range(input.shape[1]):
+                for n in range(input.shape[2]):
+                    output[i,k,n,:] = torch.from_numpy(np.polyval(input[i,k,n,:].detach().cpu().numpy(), x))
         return output
 
 
-    def weight_mul(self, weight1, input):
-        transformed = torch.einsum("bix,io -> box", input, weight1)
-        return transformed
-
-
-
-
-
-
-    def transform(self, weight1, width, input, coefficient):
-        # Apply the Sumudu transform to the input
-        transformed = torch.zeros((input.shape[0],width,coefficient.shape[0]), dtype=torch.double, device=input.device)
-        for i in range(0,coefficient.shape[0]):
-            transformed[:,:,i] = input[:,:,i]*sp.factorial((2*i)+1)/width
-
-        transformed = torch.einsum("bix,iox -> bx", transformed, weight1)
-        return transformed
-    def inverse_transform(self, input, coefficient):
-        # Apply the inverse Sumudu transform to the input weights
-        transformed = torch.zeros((input.shape[0],coefficient.shape[0]), dtype=torch.double, device=input.device)
-        for i in range(0,coefficient.shape[0]):
-            transformed[:,i] = input[:,i]/(sp.factorial((2*i)+1)*width)
-        transformed = transformed.expand(width, -1, -1)
-        transformed = transformed.permute(1, 0, 2)
-
-        return transformed
-
-    def coefficient_training(self, input, coefficient):
-        start_amplitude = math.floor(input[0,0,1]/(math.sin(.05)*.05))
-        amplitude = input.shape[0]
-        output = torch.zeros((input.shape[0], coefficient.shape[0]), dtype= torch.double,device=input.device)
-        variable_amp = torch.linspace(start_amplitude, start_amplitude + amplitude - 1, steps=amplitude, device=input.device).reshape(amplitude, 1)
-        for i in range(0,coefficient.shape[0]):
-            output[:,i] = .05*((-1)**i)*(5**((2*i)+1))/sp.factorial((2*i)+1)
-        output = torch.mul(output, variable_amp)
-    def coefficient_validation(self, input, coefficient):
-        start_amplitude = math.floor(input[0,1,0].item()/(math.exp(-.05)*math.sin(.05)*.05))
-        output = torch.zeros((input.shape[0], coefficient.shape[0]), dtype= torch.double,device=input.device)
-        amplitude = input.shape[0]
-        variable_amp = torch.linspace(start_amplitude, start_amplitude + amplitude - 1, steps=amplitude, device=input.device).reshape(amplitude, 1)
-        for i in range(0,coefficient.shape[0]):
-            output[:,i] = .05*(.05**i)*((-1)**i)*(5**((2*i)+1))/sp.factorial((2*i)+1)
-        output = torch.mul(output, variable_amp)
+    def weight_mul(self, input):
+        transformed = torch.zeros_like(input, device= input.device)
+        for i in range(input.shape[3]):
+            transformed[:,:,:,i] = torch.einsum("bix,io -> box", input[:,:,:,i], self.weight1)
+        for i in range(input.shape[2]):
+            transformed[:,:,i,:] += transformed[:,:,i,:]*torch.einsum("bit,io->bot", input[:,:,i,:], self.weight2)        
         
-        return output
-
+        return transformed
     def forward(self, x):
+        x = self.coefficient_training(x, self.degree)
+        x = self.transform(x)
+        x = self.approximate_sum(self.width, x)
+        x = self.weight_mul(x)
+        x = self.coefficient_training(x, self.degree)
+        x = self.inverse_transform(x)
 
-        x = self.transform(self.weight1, width, x, self.coefficient)
 
-
-        x = self.inverse_transform(x, self.coefficient)
-
+        x = self.approximate_sum(self.width, x).float()
 
 
         return x
 
 
-class SNO1d(nn.Module):
-    def __init__(self, width, s):
-        super(SNO1d, self).__init__()
+class SNO2d(nn.Module):
+    def __init__(self, width):
+        super(LNO2d, self).__init__()
 
+        self.width = width
+        self.fc0 = nn.Linear(3, self.width) 
+
+        self.conv0 = Sumudu_Transform(self.width, self.width, degree, self.width, s)
+        self.w0 = nn.Conv2d(self.width, self.width, 1)
+        self.norm = nn.InstanceNorm2d(self.width)
+
+        self.fc1 = nn.Linear(self.width, 128)
+        self.fc2 = nn.Linear(128, 1)
+
+    def forward(self,x):
+        grid = self.get_grid(x.shape, x.device)
+        x = torch.cat((x, grid), dim=-1)
+        x = self.fc0(x)
+        x = x.permute(0, 3, 1, 2)
+
+        x1 = self.norm(self.conv0(self.norm(x)))
+        x2 = self.w0(x)
+        x = x1 +x2
+
+        x = x.permute(0, 2, 3, 1)
+        x = self.fc1(x)
+        x = torch.sin(x)
+        x = self.fc2(x)
+        return x
+
+    def get_grid(self, shape, device):
+        batchsize, size_x, size_y = shape[0], shape[1], shape[2]
+        gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float)
+        gridx = gridx.reshape(1, size_x, 1, 1).repeat([batchsize, 1, size_y, 1])
+        gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
+        gridy = gridy.reshape(1, 1, size_y, 1).repeat([batchsize, size_x, 1, 1])
+        return torch.cat((gridx, gridy), dim=-1).to(device)
+    def __init__(self, degree, width, s):
+        super(SNO1d, self).__init__()
+        self.degree = degree
         self.s = s
         self.width = width
 
         self.fc0 = nn.Linear(1, self.width) 
+        self.layer_norm = nn.LayerNorm(self.width)
 
-        self.conv0 = Sumudu_Transform(self.width, self.width, self.width, self.s)
-        self.conv1 = Sumudu_Transform(self.width, self.width, self.width, self.s)
-        self.conv2 = Sumudu_Transform(self.width, self.width, self.width, self.s)
+        self.conv0 = Sumudu_Transform(self.width, self.width, self.degree, self.width, self.s)
+        self.conv1 = Sumudu_Transform(self.width, self.width, self.degree, self.width, self.s)
         
         self.w0 = nn.Conv1d(self.width, self.width, 1)
         self.w1 = nn.Conv1d(self.width, self.width, 1)
-        self.w2 = nn.Conv1d(self.width, self.width, 1)
-
-        self.bn0 = torch.nn.BatchNorm1d(self.width)
-        self.bn1 = torch.nn.BatchNorm1d(self.width)
-        self.bn2 = torch.nn.BatchNorm1d(self.width)
-        self.bn3 = torch.nn.BatchNorm1d(self.width)
 
         self.fc1 = nn.Linear(self.width, 128)
         self.fc2 = nn.Linear(128, 1)
-        return output
-    def approximate_sum(self, width, input, coefficient, s):
-        input = input.to(torch.float32)
-        discretization = torch.linspace(0, (s-1)*.01, steps = s, device=input.device)
-        discretization = discretization.expand(coefficient, -1)
-        for i in range(0,coefficient):
-            discretization[i,:] = torch.pow(discretization[i,:],i - 10)
-        output = torch.einsum("bix, xj->bij", input, discretization)
-
-        return output
-
-
 
     def forward(self,x):
         #grid = self.get_grid(x.shape, x.device)
         #x = torch.cat((x, grid), dim=-1)
-
         x = self.fc0(x)
         x = x.permute(0, 2, 1)
-        if x in train_loader:
-            x = self.coefficient_training(x, degree)
-        else:
-            x = self.coefficient_validation(x, degree)
-        x1 = x.float()
-
-        x1 = self.conv0(x1)
-
+        x1 = self.conv0(x)
         x2 = self.w0(x)
+
 
         x = x1 + x2
         x = F.leaky_relu(x)
-
-        x1 = x.float()
-        x1 = self.conv1(x1)
+        x1 = self.conv1(x)
         x2 = self.w1(x)
-        x = x1 + x2
 
-        x = self.approximate_sum(self.width, x, degree, s)
+        x = x1 + x2
 
 
 
         x = x.permute(0, 2, 1)
         x = self.fc1(x)
-        x = torch.sin(x)
+        x = torch.tanh(x)
         x = self.fc2(x)
         return x
   
@@ -226,56 +237,52 @@ class SNO1d(nn.Module):
 # ====================================
 #  Define parameters and Load data
 # ====================================
-s = 2048
-degree = 30
+degree = 15
+s = 50
+ntrain = 200
+nvali = 50
+ntest=130
 
+batch_size_train = 50
+batch_size_vali = 50
 
-batch_size_train = 20
-batch_size_vali = 20
-
+learning_rate = 0.002
 
 epochs = 1000
 step_size = 100
 gamma = 0.5
 
-learning_rate = .002
+modes1 = 4  
+modes2 = 4   
+width = 16
 
-
-
-width = 4
-
-
-
-
-reader = MatReader(r'C:\Users\benze\Downloads\Duffing SNO\1D_Duffing\Data\datac05.mat')
+reader = MatReader(r'C:\Users\benze\Downloads\SNO main\Beam_SNO\Data\data.beam.mat')
 x_train = reader.read_field('f_train')
 y_train = reader.read_field('u_train')
-grid_x_train = reader.read_field('x_train')
+T = reader.read_field('t')
+X = reader.read_field('x')
 
 x_vali = reader.read_field('f_vali')
 y_vali = reader.read_field('u_vali')
-grid_x_vali = reader.read_field('x_vali')
 
 x_test = reader.read_field('f_test')
 y_test = reader.read_field('u_test')
-grid_x_test = reader.read_field('x_test') 
 
-x_train = x_train.reshape(x_train.shape[0],s,1)
-x_vali = x_vali.reshape(x_vali.shape[0],s,1)
-x_test = x_test.reshape(x_test.shape[0],s,1)
+x_train = x_train.reshape(ntrain,x_train.shape[1],x_train.shape[2],1)
+x_vali = x_vali.reshape(nvali,x_vali.shape[1],x_vali.shape[2],1)
+x_test = x_test.reshape(ntest,x_test.shape[1],x_test.shape[2],1)
 
-train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_train, y_train), batch_size=batch_size_train, shuffle=True)
-vali_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_vali, y_vali), batch_size=batch_size_vali, shuffle=True)
-
+train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_train, y_train), batch_size=batch_size_train, shuffle=False)
+vali_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_vali, y_vali), batch_size=batch_size_vali, shuffle=False)
 # model
-model = SNO1d(width,s).cuda()
-
+model = SNO2d(width).cuda()
 
 # ====================================
 # Training 
 # ====================================
-optimizer = Adam(model.parameters(), lr=learning_rate, weight_decay=0)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
+torch.autograd.set_detect_anomaly(True)
+optimizer = Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, epochs)
 start_time = time.time()
 myloss = LpLoss(size_average=True)
 
@@ -296,8 +303,7 @@ for ep in range(epochs):
         out = model(x)   
         mse = F.mse_loss(out.view(batch_size_train, -1), y.view(batch_size_train, -1), reduction='mean')
         l2 = myloss(out.view(batch_size_train, -1), y.view(batch_size_train, -1))
-        mse.backward()
-
+        l2.backward()
         optimizer.step()
         train_mse += mse.item()
         train_l2 += l2.item()
@@ -393,5 +399,4 @@ ax.set_yscale('log')
 ax.set_ylabel('Loss')
 ax.set_xlabel('Epochs')
 ax.legend(loc='upper left')
-
 fig.savefig(save_results_to+'loss_history.png')
