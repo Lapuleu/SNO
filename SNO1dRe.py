8c83180bd13bb3e402828bbc981e3037bd629967
Saibilila Abudukelimu <153378724+Lapuleu@users.noreply.github.com>
2025-10-02 20:09:38 -0400

    Update SNO1dRe.py

--- SNO1dRe.py
+++ SNO1dRe.py
@@ -168,178 +168,161 @@ class SNO1d(nn.Module):
 # ====================================
 #  Define parameters and Load data
 # ====================================
+s = 2048
+batch_size_train = 20
+batch_size_vali = 20
+epochs = 1000
+step_size = 100
+gamma = 0.5
+learning_rate = .001
+width = 16
+degree = 4
 
-class main():
-    def __init__(self, degree):
-        self.degree = degree
+reader = MatReader('/workspace/gridSearch/Data/data.mat')
+x_train = reader.read_field('f_train')
+y_train = reader.read_field('u_train')
+grid_x_train = reader.read_field('x_train')
 
-    def train(self):
-        s = 2048
-        batch_size_train = 20
-        batch_size_vali = 20
-        epochs = 1000
-        step_size = 100
-        gamma = 0.5
-        learning_rate = .001
-        width = 16
+x_vali = reader.read_field('f_vali')
+y_vali = reader.read_field('u_vali')
+grid_x_vali = reader.read_field('x_vali')
 
-        reader = MatReader('/workspace/gridSearch/Data/data.mat')
-        x_train = reader.read_field('f_train')
-        y_train = reader.read_field('u_train')
-        grid_x_train = reader.read_field('x_train')
+x_test = reader.read_field('f_test')
+y_test = reader.read_field('u_test')
+grid_x_test = reader.read_field('x_test') 
 
-        x_vali = reader.read_field('f_vali')
-        y_vali = reader.read_field('u_vali')
-        grid_x_vali = reader.read_field('x_vali')
+x_train = x_train.reshape(x_train.shape[0],s,1)
+x_vali = x_vali.reshape(x_vali.shape[0],s,1)
+x_test = x_test.reshape(x_test.shape[0],s,1)
 
-        x_test = reader.read_field('f_test')
-        y_test = reader.read_field('u_test')
-        grid_x_test = reader.read_field('x_test') 
+train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_train, y_train), batch_size=batch_size_train, shuffle=True)
+vali_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_vali, y_vali), batch_size=batch_size_vali, shuffle=True)
 
-        x_train = x_train.reshape(x_train.shape[0],s,1)
-        x_vali = x_vali.reshape(x_vali.shape[0],s,1)
-        x_test = x_test.reshape(x_test.shape[0],s,1)
+# model
+model = SNO1d(degree, width,s).to(device)
 
-        train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_train, y_train), batch_size=batch_size_train, shuffle=True)
-        vali_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_vali, y_vali), batch_size=batch_size_vali, shuffle=True)
+# ====================================
+# Training 
+# ====================================
+torch.autograd.set_detect_anomaly(True)
+optimizer = Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, epochs)
+start_time = time.time()
+myloss = LpLoss(size_average=True)
 
-        # model
-        model = SNO1d(self.degree, width,s).to(device)
-        best_val = float("inf")
-        patience = 200   # stop after 200 epochs without improvement
-        counter = 0
+train_error = np.zeros((epochs, 1))
+train_loss = np.zeros((epochs, 1))
+vali_error = np.zeros((epochs, 1))
+vali_loss = np.zeros((epochs, 1))
+for ep in range(epochs):
+    model.train()
+    t1 = default_timer()
+    train_mse = 0
+    train_l2 = 0
+    n_train=0
+    for x, y in train_loader:
+        x, y = x.to(device, non_blocking=True), y.to(device, non_blocking=True)
+        t=grid_x_train.to(device, non_blocking=True)
+        optimizer.zero_grad()
+        out = model(x)   
+        mse = F.mse_loss(out.view(batch_size_train, -1), y.view(batch_size_train, -1), reduction='mean')
+        l2 = myloss(out.view(batch_size_train, -1), y.view(batch_size_train, -1))
+        l2.backward()
+        optimizer.step()
+        train_mse += mse.item()
+        train_l2 += l2.item()
+        n_train += 1
 
-        # ====================================
-        # Training 
-        # ====================================
-        torch.autograd.set_detect_anomaly(True)
-        optimizer = Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
-        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, epochs)
-        start_time = time.time()
-        myloss = LpLoss(size_average=True)
+    scheduler.step()
+    model.eval()
+    vali_mse = 0.0
+    vali_l2 = 0.0
+    with torch.no_grad():
+        n_vali=0
+        for x, y in vali_loader:
+            x, y = x.cuda(), y.cuda()
+            t=grid_x_vali.cuda()
+            out = model(x)
+            mse=F.mse_loss(out.view(batch_size_vali, -1), y.view(batch_size_vali, -1), reduction='mean')
+            vali_l2 += myloss(out.view(batch_size_vali, -1), y.view(batch_size_vali, -1)).item()
+            vali_mse += mse.item()
+            n_vali += 1
 
-        train_error = np.zeros((epochs, 1))
-        train_loss = np.zeros((epochs, 1))
-        vali_error = np.zeros((epochs, 1))
-        vali_loss = np.zeros((epochs, 1))
-        for ep in range(epochs):
-            model.train()
-            t1 = default_timer()
-            train_mse = 0
-            train_l2 = 0
-            n_train=0
-            for x, y in train_loader:
-                x, y = x.to(device, non_blocking=True), y.to(device, non_blocking=True)
-                t=grid_x_train.to(device, non_blocking=True)
-                optimizer.zero_grad()
-                out = model(x)   
-                mse = F.mse_loss(out.view(batch_size_train, -1), y.view(batch_size_train, -1), reduction='mean')
-                l2 = myloss(out.view(batch_size_train, -1), y.view(batch_size_train, -1))
-                l2.backward()
-                optimizer.step()
-                train_mse += mse.item()
-                train_l2 += l2.item()
-                n_train += 1
+    train_mse /= n_train
+    vali_mse /= n_vali
+    train_l2 /= n_train
+    vali_l2 /= n_vali
+    train_error[ep,0] = train_l2
+    vali_error[ep,0] = vali_l2
+    train_loss[ep,0] = train_mse
+    vali_loss[ep,0] = vali_mse
+    t2 = default_timer()
+    print("Epoch: %d, time: %.3f, Train Loss: %.3e,Vali Loss: %.3e, Train l2: %.4f, Vali l2: %.4f" % (ep, t2-t1, train_mse, vali_mse,train_l2, vali_l2))
+elapsed = time.time() - start_time
+print("\n=============================")
+print("Training done...")
+print('Training time: %.3f'%(elapsed))
+print("=============================\n")
 
-            scheduler.step()
-            model.eval()
-            vali_mse = 0.0
-            vali_l2 = 0.0
-            with torch.no_grad():
-                n_vali=0
-                for x, y in vali_loader:
-                    x, y = x.cuda(), y.cuda()
-                    t=grid_x_vali.cuda()
-                    out = model(x)
-                    mse=F.mse_loss(out.view(batch_size_vali, -1), y.view(batch_size_vali, -1), reduction='mean')
-                    vali_l2 += myloss(out.view(batch_size_vali, -1), y.view(batch_size_vali, -1)).item()
-                    vali_mse += mse.item()
-                    n_vali += 1
+# ====================================
+# saving settings
+# ====================================
+x = np.linspace(0, epochs-1, epochs)
+np.savetxt(save_results_to+'/epoch.txt', x)
+np.savetxt(save_results_to+'/train_loss.txt', train_loss)
+np.savetxt(save_results_to+'/vali_loss.txt', vali_loss)
+np.savetxt(save_results_to+'/train_error.txt', train_error)
+np.savetxt(save_results_to+'/vali_error.txt', vali_error)    
+save_models_to = save_results_to +"model/"
+if not os.path.exists(save_models_to):
+    os.makedirs(save_models_to)
+    
+torch.save(model, save_models_to+'Wave_states')
 
-            train_mse /= n_train
-            vali_mse /= n_vali
-            train_l2 /= n_train
-            vali_l2 /= n_vali
-            train_error[ep,0] = train_l2
-            vali_error[ep,0] = vali_l2
-            train_loss[ep,0] = train_mse
-            vali_loss[ep,0] = vali_mse
-            if vali_l2 < best_val:
-                best_val = vali_l2
-                counter = 0
-            else:
-                counter += 1
-                if counter >= patience:
-                    print(f"Early stopping at epoch {ep}")
-                    break
-            t2 = default_timer()
-            print("Epoch: %d, time: %.3f, Train Loss: %.3e,Vali Loss: %.3e, Train l2: %.4f, Vali l2: %.4f" % (ep, t2-t1, train_mse, vali_mse,train_l2, vali_l2))
-        elapsed = time.time() - start_time
-        print("\n=============================")
-        print("Training done...")
-        print('Training time: %.3f'%(elapsed))
-        print("=============================\n")
+# ====================================
+# testing
+# ====================================
+pred = torch.zeros(y_test.shape)
+index = 0
+test_l2 = 0.0
+test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_test, y_test),
+                                        batch_size=1, shuffle=False)
 
-        # ====================================
-        # saving settings
-        # ====================================
-        x = np.linspace(0, epochs-1, epochs)
-        np.savetxt(save_results_to+'/epoch.txt', x)
-        np.savetxt(save_results_to+'/train_loss.txt', train_loss)
-        np.savetxt(save_results_to+'/vali_loss.txt', vali_loss)
-        np.savetxt(save_results_to+'/train_error.txt', train_error)
-        np.savetxt(save_results_to+'/vali_error.txt', vali_error)    
-        save_models_to = save_results_to +"model/"
-        if not os.path.exists(save_models_to):
-            os.makedirs(save_models_to)
-            
-        torch.save(model, save_models_to+'Wave_states')
+with torch.no_grad():
+    for x, y in test_loader:
+        x, y = x.cuda(), y.cuda()
+        t=grid_x_test.cuda()
+        out = model(x).view(1,-1)
+        pred[index]= out
+        test_l2 += myloss(out, y).item()
+        index = index + 1
+test_l2/=index
 
-        # ====================================
-        # testing
-        # ====================================
-        pred = torch.zeros(y_test.shape)
-        index = 0
-        test_l2 = 0.0
-        test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_test, y_test),
-                                                batch_size=1, shuffle=False)
-
-        with torch.no_grad():
-            for x, y in test_loader:
-                x, y = x.cuda(), y.cuda()
-                t=grid_x_test.cuda()
-                out = model(x).view(1,-1)
-                pred[index]= out
-                test_l2 += myloss(out, y).item()
-                index = index + 1
-        test_l2/=index
-
-        scipy.io.savemat(save_results_to+'wave_states_test.mat', 
-                            mdict={'test_err': test_l2,
-                                    'x_test': grid_x_train.numpy(),
-                                    'y_test': y_test.numpy(), 
-                                    'y_pred': pred.cpu().numpy()})  
-            
-                
-            
-        print("\n=============================")
-        print('Testing error: %.3e'%(test_l2))
-        print("=============================\n")
+scipy.io.savemat(save_results_to+'wave_states_test.mat', 
+                    mdict={'test_err': test_l2,
+                            'x_test': grid_x_train.numpy(),
+                            'y_test': y_test.numpy(), 
+                            'y_pred': pred.cpu().numpy()})  
+    
+        
+    
+print("\n=============================")
+print('Testing error: %.3e'%(test_l2))
+print("=============================\n")
 
 
-        # Plotting the loss history
-        num_epoch = epochs
-        epoch = np.linspace(1, num_epoch, num_epoch)
-        fig = plt.figure(constrained_layout=False, figsize=(7, 7))
-        gs = fig.add_gridspec(1, 1)
-        ax = fig.add_subplot(gs[0])
-        ax.plot(epoch, train_loss[:,0], color='blue', label='Train Loss')
-        ax.plot(epoch, vali_loss[:,0], color='red', label='Validation Loss')
-        ax.set_yscale('log')
-        ax.set_ylabel('Loss')
-        ax.set_xlabel('Epochs')
-        ax.legend(loc='upper left')
-        fig.savefig(save_results_to+'loss_history.png')
-        return float(best_val)
-main(degree=15).train()
+# Plotting the loss history
+num_epoch = epochs
+epoch = np.linspace(1, num_epoch, num_epoch)
+fig = plt.figure(constrained_layout=False, figsize=(7, 7))
+gs = fig.add_gridspec(1, 1)
+ax = fig.add_subplot(gs[0])
+ax.plot(epoch, train_loss[:,0], color='blue', label='Train Loss')
+ax.plot(epoch, vali_loss[:,0], color='red', label='Validation Loss')
+ax.set_yscale('log')
+ax.set_ylabel('Loss')
+ax.set_xlabel('Epochs')
+ax.legend(loc='upper left')
+fig.savefig(save_results_to+'loss_history.png')
+
 
